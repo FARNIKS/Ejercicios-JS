763d097b7333f95ac80d3e79b8d5c742f6157f0e
FARNIKS <migueljimenez021405@gmail.com>
2025-12-05 17:08:36 -0500

    fix: converciones del conversor

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,9 +1,83 @@
 let optionSelect = document.getElementById("conversionType");
+let fromUnit = document.getElementById("fromUnit");
+let toUnit = document.getElementById("toUnit");
+let inputElement = document.getElementById("inputValue"); // Mantener la referencia al elemento
+let result = document.getElementById("result");
+let buttonConvert = document.getElementById("buttonConvert");
 
-let selectedOption = "length";
+let lengthUnits = ["Meters", "Kilometers", "Miles", "Yards"];
+let weightUnits = ["Grams", "Kilograms", "Pounds", "Ounces"];
+let temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"];
+let timeUnits = ["Seconds", "Minutes", "Hours", "Days"];
+
+const lengthConverter = () => {
+  const numInput = parseFloat(inputElement.value);
+  console.log(numInput);
+};
+
+let units = (unitsConversor) => {
+  fromUnit.innerHTML = "";
+  toUnit.innerHTML = "";
+
+  // Añadir placeholder (Buena práctica)
+  const placeholderText = "--- Select Unit ---";
+  const createPlaceholder = (text) => {
+    let p = document.createElement("option");
+    p.text = text;
+    p.value = "";
+    p.disabled = true;
+    p.selected = true;
+    return p;
+  };
+  fromUnit.add(createPlaceholder(placeholderText));
+  toUnit.add(createPlaceholder(placeholderText));
+
+  for (unitsValue of unitsConversor) {
+    let optionFrom = document.createElement("option");
+    optionFrom.text = unitsValue;
+    optionFrom.value = unitsValue;
+    fromUnit.add(optionFrom);
+    toUnit.add(optionFrom.cloneNode(true));
+  }
+};
+
 optionSelect.addEventListener("change", function () {
   selectedOption = optionSelect.value;
 
-  console.log(selectedOption);
+  switch (selectedOption) {
+    case "length":
+      units(lengthUnits);
+      break;
+    case "weight":
+      units(weightUnits);
+      break;
+    case "temperature":
+      units(temperatureUnits);
+      break;
+    case "time":
+      units(timeUnits);
+      break;
+
+    default:
+      break;
+  }
 });
-console.log(selectedOption);
+
+buttonConvert.addEventListener("click", function () {
+  selectedOption = optionSelect.value;
+
+  switch (selectedOption) {
+    case "length":
+      lengthConverter();
+      break;
+    case "weight":
+      break;
+    case "temperature":
+      break;
+    case "time":
+      break;
+
+    default:
+      break;
+  }
+});
