6f56a8197a27790fe2ac49ff0248fa33d8ac248b
Miguel Angel Jimenez Fajardo <128429185+FARNIKS@users.noreply.github.com>
2026-01-06 20:10:19 -0500

    Se añade ventana principal para los proyectos y avanzamos en el proyecto de snake

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Ejercicios-JS
+
+Son distintos ejercicios en JS, HTML y CSS los cuales se podran visualizar en una sola pagina con suus propias paginas y quue es lo que uno aprende en ello
+En los proyectos esta;
+
+- Contador
+- Formulario html
+- Conversor
---
+++ Snake/script.js
@@ -0,0 +1,60 @@
+const board = document.getElementById("board");
+const scoreBoard = document.getElementById("scoreBoard");
+const startButton = document.getElementById("start");
+const gameOver = document.getElementById("gameOver");
+
+const boardSize = 10;
+const gameSpeed = 100;
+const squareTypes = {
+  emptySquare: 0,
+  snakeSquare: 1,
+  foodSquare: 2,
+};
+
+const directions = {
+  ArrawUp: -10,
+  ArrawDown: 10,
+  ArrawLeft: -1,
+  ArrawRight: 1,
+};
+
+// Variables de juego
+let snake;
+let score;
+let direction;
+let boardSquares;
+let emptySquares;
+let moveInterval;
+
+const createBoard = () => {
+  boardSquares.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      const squareValue = `${rowIndex}${columnIndex}`;
+      const squareElement = document.createElement("div");
+      squareElement.setAttribute("class", "square emptySquare");
+      squareElement.setAttribute("id", squareValue);
+      board.appendChild(squareElement);
+      emptySquares.push(squareValue);
+    });
+  });
+};
+
+const setGame = () => {
+  snake = ["00", "01", "02", "03"];
+  score = snake.length;
+  direction = "ArrowRight";
+  boardSquares = Array.from(Array(boardSize), () =>
+    new Array(boardSize).fill(squareTypes.emptySquare)
+  );
+  console.log(boardSquares);
+  board.innerHTML = "";
+  emptySquares = [];
+
+  createBoard();
+};
+
+const startGame = () => {
+  setGame();
+};
+
+startButton.addEventListener("click", startGame);
