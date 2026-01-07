94af10142036e33d085134ee2e6ca89668a2235a
Miguel Angel Jimenez Fajardo <128429185+FARNIKS@users.noreply.github.com>
2026-01-07 18:15:44 -0500

    Se termina el proyecto lo quue faltaria seria agregar ventana con lo aprendido para hacerlo de manera mas interactiva y llamativa, ademas nos sirve de repaso

--- Snake/script.js
+++ Snake/script.js
@@ -1,10 +1,11 @@
 const board = document.getElementById("board");
 const scoreBoard = document.getElementById("scoreBoard");
 const startButton = document.getElementById("start");
-const gameOver = document.getElementById("gameOver");
+const gameOverSing = document.getElementById("gameOver");
+const snakeImage = document.getElementById("snakeImage");
 
 const boardSize = 10;
-const gameSpeed = 100;
+const gameSpeed = 150;
 const squareTypes = {
   emptySquare: 0,
   snakeSquare: 1,
@@ -12,10 +13,10 @@ const squareTypes = {
 };
 
 const directions = {
-  ArrawUp: -10,
-  ArrawDown: 10,
-  ArrawLeft: -1,
-  ArrawRight: 1,
+  ArrowUp: -10,
+  ArrowDown: 10,
+  ArrowLeft: -1,
+  ArrowRight: 1,
 };
 
 // Variables de juego
@@ -26,11 +27,101 @@ let boardSquares;
 let emptySquares;
 let moveInterval;
 
+const updateScore = () => {
+  scoreBoard.innerText = score;
+};
+
+const drawSnake = () => {
+  snake.forEach((square) => drawSquare(square, "snakeSquare"));
+};
+
 /*@params
 square> posicion del cuadrado
 type> tipo de cuadrado (empty, snake, food)
  */
-const drawSquare = (square, type) => {};
+const drawSquare = (square, type) => {
+  const [row, column] = square.split("");
+  boardSquares[row][column] = squareTypes[type];
+  const squareElement = document.getElementById(square);
+  squareElement.setAttribute("class", `square ${type}`);
+
+  if (type === "emptySquare") {
+    emptySquares.push(square);
+  } else {
+    if (emptySquares.indexOf(square) !== -1) {
+      emptySquares.splice(emptySquares.indexOf(square), 1);
+    }
+  }
+};
+
+const moveSnake = () => {
+  const newSquare = String(
+    Number(snake[snake.length - 1]) + directions[direction]
+  ).padStart(2, "0");
+
+  const [row, column] = newSquare.split("");
+
+  if (
+    newSquare < 0 ||
+    newSquare > boardSize * boardSize ||
+    (direction === "ArrowRight" && column == 0) ||
+    (direction === "ArrowLeft" && column == 9) ||
+    boardSquares[row][column] === squareTypes.snakeSquare
+  ) {
+    gameOver();
+  } else {
+    snake.push(newSquare);
+
+    if (boardSquares[row][column] === squareTypes.foodSquare) {
+      addFood();
+    } else {
+      const emptySquare = snake.shift();
+      drawSquare(emptySquare, "emptySquare");
+    }
+    drawSnake();
+  }
+};
+
+const addFood = () => {
+  score++;
+  updateScore();
+  createRandomFood();
+};
+
+const gameOver = () => {
+  gameOverSing.style.display = "block";
+  clearInterval(moveInterval);
+  startButton.disabled = false;
+};
+
+const setDirection = (newDirection) => {
+  direction = newDirection;
+};
+
+const directionEvent = (key) => {
+  switch (key.code) {
+    case "ArrowUp":
+      direction !== "ArrowDown" && setDirection(key.code);
+      break;
+    case "ArrowDown":
+      direction !== "ArrowUp" && setDirection(key.code);
+      break;
+    case "ArrowLeft":
+      direction !== "ArrowRight" && setDirection(key.code);
+      break;
+    case "ArrowRight":
+      direction !== "ArrowLeft" && setDirection(key.code);
+      break;
+  }
+};
+
+const createRandomFood = () => {
+  const ramdomEmptySquare =
+    emptySquares[Math.floor(Math.random() * emptySquares.length)];
+  drawSquare(ramdomEmptySquare, "foodSquare");
+  emptySquares[Math.floor(Math.random() * emptySquares.length)];
+  drawSquare(ramdomEmptySquare, "foodSquare");
+};
 
 const createBoard = () => {
   boardSquares.forEach((row, rowIndex) => {
@@ -47,7 +138,7 @@ const createBoard = () => {
 
 const setGame = () => {
   snake = ["00", "01", "02", "03"];
-  score = snake.length;
+  score = snake.length - 4;
   direction = "ArrowRight";
   boardSquares = Array.from(Array(boardSize), () =>
     new Array(boardSize).fill(squareTypes.emptySquare)
@@ -61,9 +152,14 @@ const setGame = () => {
 
 const startGame = () => {
   setGame();
-  gameOver.style.display = "none";
+  gameOverSing.style.display = "none";
+  snakeImage.style.display = "none";
   startButton.disabled = true;
   drawSnake();
+  updateScore();
+  createRandomFood();
+  document.addEventListener("keydown", directionEvent);
+  moveInterval = setInterval(() => moveSnake(), gameSpeed);
 };
 
 startButton.addEventListener("click", startGame);
